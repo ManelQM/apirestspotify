0b609414a646a8a12e50bfa699e19604fd0a9a03
Manel <quemonmanel@gmail.com>
2024-03-05 19:05:44 +0100

    feat:authmiddleware works

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 const express = require("express"); 
 const router = express.Router();
 const UserController = require("../controllers/user");
+const authorization = require("../middleware/authMiddleware"); 
 
 router.get("/demouser", UserController.prueba);
 router.post("/register", UserController.register);
 router.get("/login", UserController.login);
-router.get("/getProfile/:id", UserController.getProfile); 
+router.get("/getProfile/:id", authorization.auth,UserController.getProfile); 
 
 module.exports = router;  
