17b06290653566dcafae392b7395d1f3bb6ec809
Manel <quemonmanel@gmail.com>
2024-03-20 22:58:06 +0100

    feat: getallartistalbums controller works

--- controllers/album.js
+++ controllers/album.js
@@ -1,3 +1,4 @@
+const album = require("../models/album");
 const Album = require("../models/album");
 
 // RUTA PRUEBA
@@ -73,14 +74,44 @@ const getOneAlbum = async (req, res) => {
 };
 
 const getAllArtistAlbums = async (req, res) => {
+  try {
+    const artistId = req.params.id;
 
-  try{
+    let page = 1;
 
-  }catch{
+    if (req.params.page) page = req.params.page;
 
+    const itemsPerPage = 10;
+
+    const allArtistAlbums = await Album.find({ artist: artistId })
+      .sort({ created_at: -1 })
+      .skip((page - 1) * itemsPerPage)
+      .limit(itemsPerPage);
+
+    if (!allArtistAlbums) {
+      return res.status(404).json({
+        status: "error",
+        message: "Cant find the albums list of this artist",
+      });
+    }
+    const albumsWithArtistName = allArtistAlbums.map(album => ({
+      ...album.toObject(),
+      artist: album.artist.name //TODO: AGREGAR EL NOMBRE DE ARTIST EN LOS ALBUMS
+    }));
+    return res.status(200).json({
+      status: "success",
+      message: "Albums of this user",
+      page,
+      album: albumsWithArtistName,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(400).json({
+      status: "error",
+      message: "INTERNAL SERVER ERROR",
+    });
   }
-
-}; 
+};
 
 module.exports = {
   prueba,
--- routes/album.js
+++ routes/album.js
@@ -8,6 +8,6 @@ const authorization = require("../middleware/authMiddleware");
 router.get("/demouser",AlbumController.prueba);
 router.post("/addalbum", authorization.auth, AlbumController.createAlbum);
 router.get("/getonealbum/:id?", authorization.auth , AlbumController.getOneAlbum); 
-router.get("allartistalbums/:id?", authorization.auth, AlbumController.getAllArtistAlbums);
+router.get("/allartistalbums/:id?", authorization.auth, AlbumController.getAllArtistAlbums);
 
 module.exports = router; 
