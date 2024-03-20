5589716044fef0123de2bdfa7b32c8a63808a45e
Manel <quemonmanel@gmail.com>
2024-03-20 22:02:35 +0100

    chore: getallartistalbums

--- controllers/album.js
+++ controllers/album.js
@@ -26,7 +26,6 @@ const createAlbum = async (req, res) => {
     }
 
     let newAlbum = new Album(params);
-   
 
     const albumStored = await newAlbum.save();
     if (!albumStored) {
@@ -49,7 +48,43 @@ const createAlbum = async (req, res) => {
   }
 };
 
+const getOneAlbum = async (req, res) => {
+  try {
+    let albumId = req.params.id;
+    const album = await Album.findById(id);
+    if (!albumId || !album) {
+      return res.status(400).json({
+        status: "error",
+        message: "Cant find the album",
+      });
+    }
+    return res.status(200).json({
+      status: "success",
+      message: "This is your album",
+      album: album,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(404).json({
+      status: "error",
+      message: "INTERNAL SERVER ERROR",
+    });
+  }
+};
+
+const getAllArtistAlbums = async (req, res) => {
+
+  try{
+
+  }catch{
+
+  }
+
+}; 
+
 module.exports = {
   prueba,
   createAlbum,
+  getOneAlbum,
+  getAllArtistAlbums,
 };
