f258eeb0535d8e6034c26b2dc6c8bb9db1a7773b
Manel <quemonmanel@gmail.com>
2024-03-25 12:16:59 +0100

    feat: albumsongs list controller

--- controllers/song.js
+++ controllers/song.js
@@ -1,6 +1,5 @@
-const { get } = require("mongoose");
 const Song = require("../models/song");
-
+const mongoosePagination = require("mongoose-pagination");
 // RUTA PRUEBA
 const prueba = async (req, res) => {
   try {
@@ -69,8 +68,46 @@ const getSong = async (req, res) => {
   }
 };
 
+const getAllSongsAlbum = async (req, res) => {
+  try {
+    let albumId = req.params.id;
+
+    let page = 1;
+
+    if (req.params.id) page = req.params.page;
+
+    const itemsPerPage = 10;
+
+    const songsAlbum = await Song.find({ album: albumId })
+      .sort({ created_at: -1 })
+      .skip((page - 1) * itemsPerPage)
+      .limit(itemsPerPage)
+      .populate();
+
+    if (!songsAlbum) {
+      return res.status(404).json({
+        status: "error",
+        message: "Cant find the album songs",
+      });
+    }
+    return res.status(200).json({
+      status: "success",
+      message: "Album songs list",
+      page,
+      songsAlbum,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(400).json({
+      status: "error",
+      message: "INTERNAL SERVER ERROR",
+    });
+  }
+};
+
 module.exports = {
   prueba,
   saveSong,
   getSong,
+  getAllSongsAlbum,
 };
--- routes/song.js
+++ routes/song.js
@@ -9,5 +9,6 @@ const authorization = require("../middleware/authMiddleware");
 router.get("/demouser", SongController.prueba);
 router.post("/savesong", authorization.auth,SongController.saveSong);
 router.get("/getonesong/:id", authorization.auth,SongController.getSong); 
+router.get("/allalbumsongs/:id", authorization.auth, SongController.getAllSongsAlbum);
 
 module.exports = router; 
